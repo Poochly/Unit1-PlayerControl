94075655fa376fcba70c85e24329fe16f8b55e8c
Poochly <743530@student.dusd.net>
2023-02-17 15:28:39 -0800

    a
    a

--- Unit1-PlayerControl/Assets/Scripts/PlayerController.cs
+++ Unit1-PlayerControl/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 20.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
@@ -23,7 +26,13 @@ public class PlayerController : MonoBehaviour
 
         //Move the vehicle forward
         transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
 
     }
 }
